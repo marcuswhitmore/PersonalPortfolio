17446c967a509c050ea04f497379554537deefd2
Marcus Whitmore <marcus.whitmore@gmail.com>
2019-07-11 18:34:33 -0500

    initial commit

--- src/componets/landing/index.js
+++ src/componets/landing/index.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
 import "./style.css";
 import logo from "./logo.svg";
+import Navbar from "./navbar";
 
 class SVG extends Component {render() {
   return (
     <div className="SVG">
+      <Navbar />
       <header className="SVG-header">
         <a href="https://github.com/marcuswhitmore"><img src={logo} className="SVG-logo" alt="logo" /></a>
         <span> Marcus Whitmore</span>
